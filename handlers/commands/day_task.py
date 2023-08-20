4119349ffe3c5375079bf6312f8693179dce5313
V0D14ka <minecraft35510880@gmail.com>
2023-08-20 18:45:53 +0300

    Day task menu

--- handlers/commands/day_task.py
+++ handlers/commands/day_task.py
@@ -5,20 +5,35 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 from aiogram.utils.exceptions import MessageCantBeDeleted, CantInitiateConversation, BotBlocked, Unauthorized
 from aiogram.dispatcher import FSMContext
 
-from create_bot import bot
+from create_bot import bot, day_task_menu
 from static import messages
 
 
 async def day_task(message: Union[types.CallbackQuery, types.Message]):
-
+    markup = await day_task_menu.menu_keyboard()
     if isinstance(message, types.CallbackQuery):
         call = message
-        await call.message.edit_text("Выбрано day_task")
-        # await call.message.edit_reply_markup(markup)
+        await call.message.edit_text("Тут будет какое-то задание")
+        await call.message.edit_reply_markup(markup)
 
     if isinstance(message, types.Message):
-        await message.answer("Выбрано day_task")
+        await message.answer("Тут будет какое-то задание", reply_markup=markup)
+
+
+async def menu_navigate(call: types.CallbackQuery, state: FSMContext, callback_data: dict):
+    """
+        Навигация по меню knowledge
+    """
+    # Достаем информацию из колбека
+    current_level = callback_data.get('level')  # Текущий уровень меню.
+
+    # Смотрим какой уровень был вызван
+    match current_level:
+
+        case "0":
+            await day_task(call)
 
 
 def register_day_task_handlers(_dp: Dispatcher):
     _dp.register_message_handler(day_task, commands=['day_task'])
+    _dp.register_callback_query_handler(menu_navigate, day_task_menu.menu_cd.filter(), state=None)
