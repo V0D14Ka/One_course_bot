8f83a56f6f2a67bc644c9d6fbc97b0ac46c68a45
V0D14ka <minecraft35510880@gmail.com>
2023-08-31 19:38:32 +0300

    Async upload on google_drive

--- services/google_api.py
+++ services/google_api.py
@@ -1,3 +1,4 @@
+import mimetypes
 import os
 import datetime
 from datetime import timedelta
@@ -5,6 +6,7 @@ import asyncio
 import json
 import pprint
 
+import aiohttp
 from dotenv import load_dotenv
 from aiogoogle import Aiogoogle
 from googleapiclient.errors import HttpError
@@ -162,3 +164,46 @@ class GoogleAPI:
 
         except Exception as e:
             print('An error occurred: %s' % e)
+
+    async def upload(self, file_url, username):
+        folder_id = "1KU8WOgqxc9LmVonxF2IEdT9bkYyQBqce"
+        metadata = {
+            "name": username,
+            "parents": [folder_id],
+        }
+        async with self.aiogoogle as g:
+            drive_v3 = await g.discover("drive", "v3")
+            async with aiohttp.ClientSession():
+                await g.as_service_account(drive_v3.files.create(
+                        json=metadata,
+                        upload_file=file_url,
+                ))
+
+
+async def upload_file(full_path, new_name):
+    SERVICE_ACCOUNT_FILE = 'onecourseproject.json'
+    creds_data = load_creds_from_file(SERVICE_ACCOUNT_FILE)
+    async with Aiogoogle(service_account_creds=creds_data) as aiogoogle:
+        # Create API
+        drive_v3 = await aiogoogle.discover("drive", "v3")
+
+        req = drive_v3.files.create(
+            upload_file=full_path,
+            fields="id",
+            json={"name": new_name}
+        )
+
+        # Usually autodetected by Drive
+        # mimetypes autodetects the mimetype by file extension, which isn't always accurate.
+        # You may want to manually enter this for some extra assurance.
+        req.upload_file_content_type = mimetypes.guess_type(full_path)[0]
+
+        # Upload file
+        upload_res = await aiogoogle.as_service_account(req)
+        print("Uploaded {} successfully.\nFile ID: {}".format(full_path, upload_res['id']))
+        # file_id = upload_res["id"]
+        # # Rename uploaded file
+        # await aiogoogle.as_user(
+        #     drive_v3.files.update(fileId=file_id, json={"name": new_name})
+        # )
+        # print("Renamed {} to {} successfully!".format(full_path, new_name))
