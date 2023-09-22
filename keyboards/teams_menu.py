fa9d477b91f6e341e50f1a8487161871988dc5fc
V0D14ka <minecraft35510880@gmail.com>
2023-09-22 13:13:49 +0300

    Case when participant wants to leave team

--- keyboards/teams_menu.py
+++ keyboards/teams_menu.py
@@ -4,13 +4,13 @@ from aiogram.utils.callback_data import CallbackData
 
 class TeamsMenu:
     """
-        Класс отображения клавиатур knowledge
+        Класс отображения клавиатур Teams
     """
     menu_cd = CallbackData("teams_menu", "level", "category", "method_id", "team_id")
 
     def make_callback_data(self, level, category=0, method_id=0, team_id=0):
         """
-            Создание callback меню knowledge
+            Создание callback меню Teams
         """
         return self.menu_cd.new(level=level, category=category, method_id=method_id, team_id=team_id)
 
@@ -23,50 +23,31 @@ class TeamsMenu:
         markup = InlineKeyboardMarkup()
         if flag:
             markup.row(
-                InlineKeyboardButton(text="Создать группу", callback_data=self.make_callback_data(
+                InlineKeyboardButton(text="Создать команду", callback_data=self.make_callback_data(
                     level=current_level + 1,
                     category=1, method_id=1))
             )
 
             markup.row(
-                InlineKeyboardButton(text="Вступить в группу", callback_data=self.make_callback_data(
+                InlineKeyboardButton(text="Вступить в команду", callback_data=self.make_callback_data(
                     level=current_level + 1,
                     category=1, method_id=2))
             )
 
         else:
             markup.row(
-                InlineKeyboardButton(text="Учасники", callback_data=self.make_callback_data(
+                InlineKeyboardButton(text="Участники", callback_data=self.make_callback_data(
                     level=current_level + 1,
                     category=2, method_id=1,
                     team_id=team_id))
             )
 
             markup.row(
-                InlineKeyboardButton(text="Покинуть группу", callback_data=self.make_callback_data(
+                InlineKeyboardButton(text="Покинуть команду", callback_data=self.make_callback_data(
                     level=current_level + 1,
                     category=2, method_id=2,
                     team_id=team_id))
             )
-
-        return markup
-
-    async def choose_method(self, category, methods):
-        current_level = 1
-        markup = InlineKeyboardMarkup()
-
-        for topic in methods:
-            button_text = f"{topic[1]}"
-            callback_data = self.make_callback_data(level=current_level + 1, category=category, method_id=topic[0])
-
-            markup.row(
-                InlineKeyboardButton(text=button_text, callback_data=callback_data)
-            )
-
-        markup.row(
-            InlineKeyboardButton(text="Назад", callback_data=self.make_callback_data(level=current_level - 1,
-                                                                                     category=category))
-        )
 
         return markup
 
